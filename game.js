cf0052b59b5ff807f7d26a959cc728fb89fa0e43
saralongoS2 <125043002+saralongoS2@users.noreply.github.com>
2023-10-29 21:49:12 -0300

    third commit

--- game.js
+++ game.js
@@ -4,23 +4,27 @@ const form = document.querySelector('.login_form')
 const app = document.getElementById('app')
 const player1 =document.getElementById('player1')
 const player2 =document.getElementById('player2')
-
 const pontos1 =document.getElementById('pontos1')
 const pontos2 =document.getElementById('pontos2')
-const header = document.getElementById('header')
 const jogo = document.getElementById('jogo')
+const divgrid = document.createElement('div')
+const main = document.getElementById('main')
 
 let jogadorDaVez = 0
 let pontuacao = [0,0]
 
-const main = document.getElementById('main')
+const span1 = document.createElement('span')
+span1.setAttribute('id', 'player1')
+const span2 = document.createElement('span')
+span1.setAttribute('id', 'pontos1')
+const spantexto = document.createElement('span')
+spantexto.innerText = 'X'
+const span3 = document.createElement('span')
+span1.setAttribute('id', 'player2')
+const span4 = document.createElement('span')
+span1.setAttribute('id', 'pontos2')
 
-
-//perguntar aqui como faco pra isso valer para os dois
-
-//esse target nada mais é doq o input... eu acho
 const validateInput = ({ target }) => {
-   //se oq ta escrito no input for maior que 2 remove o "disabilitado"
     if(target.value.length>2){
         button.removeAttribute('disabled'); 
    }else{
@@ -28,21 +32,13 @@ const validateInput = ({ target }) => {
    }
 }
 
-//pega o evento de form e bloqueia ele enviar o conteudo e recarregar
-
 const handleSubmit = (event) => {
     event.preventDefault(event);
 }
 
-//input.addEventListener('input', validateInput);
-//form.addEventListener('submit', handleSubmit);
-
-
 const grid = document.querySelector('.grid');
-
 const priplayer = document.querySelector('.player1') 
 const segplayer = document.querySelector('.player2')
-
 const jogador1 = document.getElementById('jogador1')
 const jogador2 = document.getElementById('jogador2')
 
@@ -69,10 +65,7 @@ const createElement = (tag, className) => {
 let primeiCarta='';
 let segunCarta=''
 
-
 const checarFim = () => {
-    //da ppra usar o filter, talvez para filtar somento o que tem a 
-    //classe desabilitada
     const desabilitadaCartas = document.getElementsByClassName('disaparecer-carta')
     if(desabilitadaCartas.length===20){
         setTimeout(() => { alert('parabens voce ganhou um centavo') },500)
@@ -86,16 +79,12 @@ const checarCarta = () => {
    if(primeiroAnimal=== segundoAnimal){
         primeiCarta.firstChild.classList.add('disaparecer-carta')
         segunCarta.firstChild.classList.add('disaparecer-carta')
-    
         primeiCarta=''
         segunCarta=''
 
         pontuacao[jogadorDaVez]++
-
         mostraPontuacao()
-
         checarFim()
-
     } else{
         jogadorDaVez = jogadorDaVez === 0 ? 1 : 0
         setTimeout(() => {
@@ -110,58 +99,63 @@ const checarCarta = () => {
 }
 
 const revealCard= ({target}) => {
-   
     if(target.parentNode.className.includes('revelar-carta')){
         return;
     }
-
     if(primeiCarta=== ''){
         target.parentNode.classList.add('revelar-carta')
         primeiCarta= target.parentNode;
     }else if(segunCarta===''){
         target.parentNode.classList.add('revelar-carta')
         segunCarta= target.parentNode;
-
         checarCarta()
     }
-
 }
 
 const createCard = (animais) => {
     const card = createElement('div', 'card');
     const frent = createElement('div', 'face frent');
     const back = createElement('div', 'face back');
-
     frent.style.backgroundImage = `url('${animais}')`;
 
     card.appendChild(frent);
     card.appendChild(back);
     card.addEventListener('click', revealCard)
     card.setAttribute('data-animais', animais)
-    grid.appendChild(card);
-
+    divgrid.appendChild(card);
     return card;
 }
 
-//cada passada era pra criar um personagem
 const loadGame = () => {
     app.innerHTML = ''
+    const header1 = document.createElement('header')
+    header1.setAttribute('id', 'header')
+    header1.appendChild(span1)
+    header1.appendChild(span2)
+    header1.appendChild(span3)
+    header1.appendChild(span4)
+    app.appendChild(header1)
+    divgrid.setAttribute('class', 'grid')
+    app.appendChild(divgrid)
+    main.appendChild(app)
+    main.appendChild(divgrid)
+
+    mostraPontuacao()
+
     const duplicarAnimais = [ ... animais, ... animais ];
     const embaralharCartas = duplicarAnimais.sort(()=> Math.random()- 0.5)
+    
     embaralharCartas.forEach((animais) => {
-        
-        mostraPontuacao()
         const card = createCard(animais)
-        grid.appendChild(card)
+        divgrid.appendChild(card)
     })
-    jogo.appendChild(grid)
-    //priplayer.innerHTML = localStorage.getItem('player1')
+    jogo.appendChild(divgrid)
 }
 
-
 const mostraPontuacao = () => {
-    pontos1.innerText = pontuacao[0]
-    pontos2.innerText = pontuacao[1]
+    span2.innerText = pontuacao[0]
+    spantexto. innerText = 'X'
+    span4.innerText = pontuacao[1]
 }
 
 const mostraFormulario = () => {
@@ -198,8 +192,8 @@ const mostraFormulario = () => {
     button.innerText = 'Jogar'
     button.addEventListener('click', () => {
         loadGame()
-        player1.innerText = input1.value
-        player2.innerText = input2.value
+        span1.innerText = input1.value
+        span3.innerText = input2.value
     })
 
     divLogin.appendChild(divLoginComeco)
@@ -210,6 +204,5 @@ const mostraFormulario = () => {
     app.appendChild(divLogin)
 }
 
+//init
 mostraFormulario()
-
-//loadGame();
